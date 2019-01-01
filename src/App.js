d1068fcc7932786ecf4c72157eb8139b7a17c18c
StacyLayton <Stacy.Layton@familysearch.org>
2019-01-01 10:17:13 -0700

    Changed title (to fit) and some alt tags (to get rid of warnings).

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ class App extends Component {
           <div className="right-half" onClick={this.playInstrument} align="left">
             <p>LAYTON FAMILY TIMELINE</p>
             <p>2002</p>
-            <img src="pics/TimelinePhotos/AnaandJared_Christmas2008.jpg" alt="Family Photo" className="input" width="400" />
+            <img src="pics/TimelinePhotos/AnaandJared_Christmas2008.jpg" alt="Ana and Jared Christmas 2008" className="input" width="400" />
             <p>2003</p>
-            <img src="pics/TimelinePhotos/AnaChristmasTimeCabelas.jpg" alt="Christmas Photo" className="input" width="400" />
+            <img src="pics/TimelinePhotos/AnaChristmasTimeCabelas.jpg" alt="Christmas at Cabelas" className="input" width="400" />
             <p>2004</p>
             <img src="pics/TimelinePhotos/AnaJaredandStacy_Christmas2008.jpg" alt="Christmas 2008" className="input" width="400" />
             <p>2005</p>
-            <img src="pics/TimelinePhotos/Aug2010FamilyReunionCoeurDLeneLake.jpg" alt="Aug 2010" data-sound="music/banjo.mp3" className="input" width="400" />
+            <img src="pics/TimelinePhotos/Aug2010FamilyReunionCoeurDLeneLake.jpg" alt="Family Reunion Aug 2010" data-sound="music/banjo.mp3" className="input" width="400" />
             <p>2006</p>
             <img src="pics/TimelinePhotos/BabyJaredwithAna.JPG" alt="cello" alt="Baby Jared with Ana" className="input" width="400" />
             <p>2007</p>
